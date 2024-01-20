bde068112e4ddc9d342e2dcebd9f251589a64193
Leigh Oliver <leighcharlesoliver@gmail.com>
2024-01-21 10:16:21 +1100

    Setting up poetry2nix for development and packaging

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  poetry2nix = pkgs.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "poetry2nix";
+    rev = "e0b44e9e2d3aa855d1dd77b06f067cd0e0c3860d";
+    sha256 = "sha256-puYyylgrBS4AFAHeyVRTjTUVD8DZdecJfymWJe7H438=";
+  };
+
+  devpkgs = import <nixpkgs> {
+    overlays = [ (import "${poetry2nix.outPath}/overlay.nix") ];
+  };
+
+  uniplotEnv = devpkgs.poetry2nix.mkPoetryEnv {
+      projectDir = ./.;
+      preferWheels = true;
+      editablePackageSources = {
+          uniplot = ./.;
+      };
+  };
+in uniplotEnv.env
